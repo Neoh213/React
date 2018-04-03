ea3378c3c2957083551b366827bf9dfde408bfcf
Neoh213 <djrumph@gmail.com>
2018-04-02 21:28:26 -0400

    loaded a template

--- indecision-app/public/scripts/apps.js
+++ indecision-app/public/scripts/apps.js
@@ -1,3 +1,13 @@
 console.log("app is running");
 
 //JSX - Javascript XML
+//var template = <p> This is JSX from app.js!</p>;
+
+var template = React.createElement(
+  "p",
+  null,
+  " This is JSX from app.js!"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
